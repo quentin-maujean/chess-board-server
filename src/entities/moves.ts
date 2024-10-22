36af311373d0542eef7634f63a3b79ee39c7456b
Nananne (Jitera) <developers@jitera.com>
2024-10-22 14:21:46 +0000

    fix: 3 issues from deployment report

--- src/entities/moves.ts
+++ src/entities/moves.ts
@@ -12,6 +12,9 @@ export class Move {
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   updated_at: Date;
 
+  @ManyToOne(() => Piece, (piece) => piece.moves)
+  piece: Piece;
+
   @Column({ type: 'varchar' })
   from_position: string;
 
@@ -23,8 +26,4 @@ export class Move {
 
   @Column({ type: 'timestamp' })
   move_time: Date;
-
-  @ManyToOne(() => Piece, (piece) => piece.moves)
-  @Column()
-  piece_id: number;
 }
